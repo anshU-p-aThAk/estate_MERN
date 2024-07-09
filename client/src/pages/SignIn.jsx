e897013cfa715e3833d1f98a038d8d40b29b37d9
anshU-p-aThAk <surajshiv15ss@gmail.com>
2024-07-09 12:00:56 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice.js";
 
 export default function SignIn() {
+
+    const dispatch = useDispatch();
 
     const navigate = useNavigate();
 
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const { loading, error } = useSelector((state) => state.user);
 
     const handleChange = (e) => {
         setFormData({
@@ -19,10 +22,10 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        setLoading(true);
-        setError(null);
+
 
         try {
+            dispatch(signInStart());
             const res = await fetch("/api/auth/signin", {
                 method: "POST",
                 headers: {
@@ -33,19 +36,16 @@ export default function SignIn() {
 
             const data = await res.json();
             if (data.success === false) {
-                setLoading(false);
-                setError(data.message);
+                dispatch(signInFailure(data.message))
                 return;
             }
             // Handle successful signup (e.g., redirect to login)
-            setLoading(false);
-            setError(null);
+            dispatch(signInSuccess(data))
             navigate("/");
         } catch (error) {
-            setLoading(false);
-            setError(error.message);
+            dispatch(signInFailure(error.message))
         }
-    };
+    }
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
