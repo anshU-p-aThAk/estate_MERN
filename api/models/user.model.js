e9eebfc9c42c5b52b13964bf1f3b9f6e2a01fa70
anshU-p-aThAk <surajshiv15ss@gmail.com>
2024-07-09 21:26:48 +0530

    update header

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        default: "https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg="
     }
 }, { timestamps: true });
 
